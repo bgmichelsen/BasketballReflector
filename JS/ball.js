d872b555bae43e289dab878f9b409d1fcd742c6f
Brandon Michelsen <brandongmichelsen@gmail.com>
2025-06-27 21:31:29 -0700

    Got the physics of the ball working

--- JS/ball.js
+++ JS/ball.js
@@ -1,27 +1,77 @@
 class Ball
 {
-    constructor(radius, strt_pos, launch_angle, color)
+    constructor(radius, strt_pos, launch_angle, velocity, color)
     {
         this.rad = radius;
         this.pos = { x: strt_pos.x, y: strt_pos.y };
         this.angle = launch_angle;
+        
+        // Calculate the velocity components
+        let x_vel = velocity * Math.cos(launch_angle);
+        let y_vel = velocity * Math.sin(launch_angle);
+        this.velocity = { x: x_vel, y: y_vel };
+
         this.color = color;
     }
 
-    draw(ctx)
+    draw(ctx, parameters)
     {
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        let x = this.pos.x;
-        let y = this.pos.y;
-        ctx.arc(x, y, this.rad, 0.0, 2.0 * Math.PI);
+        let x = this.pos.x * parameters.scale;
+        let y = this.pos.y * parameters.scale;
+        let r = this.rad * parameters.scale;
+        ctx.arc(x, y, r, 0.0, 2.0 * Math.PI);
         ctx.closePath();
         ctx.fill();
     }
 
     simulate(parameters)
     {
-        this.pos.x += 0.2;
-        this.pos.y += 0.2;
+        let ts = parameters.ts;
+        let gr = parameters.gravity;
+
+        this.velocity.x += gr.x * ts;
+        this.velocity.y += gr.y * ts;
+        this.pos.x += this.velocity.x * ts;
+        this.pos.y += this.velocity.y * ts;
+
+        this.handle_wall_collisions(parameters);
+        this.handle_stopping();   
+    }
+
+    handle_wall_collisions(parameters)
+    {
+        let x = this.pos.x;
+        let y = this.pos.y;
+        if ((x - this.rad) <= 0.0)
+        {
+            this.pos.x = this.rad;
+            this.velocity.x *= (-1 * parameters.damping);
+        }
+        if ((x + this.rad) >= parameters.width)
+        {
+            this.pos.x = (parameters.width - this.rad);
+            this.velocity.x *= (-1 * parameters.damping);
+        }
+        if ((y - this.rad) <= 0.0)
+        {
+            this.pos.y = this.rad;
+            this.velocity.y *= (-1 * parameters.damping);
+        }
+        if ((y + this.rad) >= parameters.height)
+        {
+            this.pos.y = (parameters.height - this.rad);
+            this.velocity.y *= (-1 * parameters.damping);
+        }
+    }
+
+    handle_stopping()
+    {
+        if (this.velocity.x < 0.1 && this.velocity < 0.1)
+        {
+            this.velocity.y = 0.0;
+            this.velocity.x = 0.0;
+        }
     }
 }
--- JS/simulator.js
+++ JS/simulator.js
@@ -5,12 +5,12 @@ var parameters = null;
 
 var ball = null;
 
-function draw(canvas, ctx, objects)
+function draw(canvas, ctx, objects, parameters)
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < objects.length; i++)
     {
-        objects[i].draw(ctx);
+        objects[i].draw(ctx, parameters);
     }
 }
 
@@ -25,7 +25,7 @@ function simulation(objects, parameters)
 function update(canvas, ctx, objects, parameters)
 {
     simulation(objects, parameters);
-    draw(canvas, ctx, objects);
+    draw(canvas, ctx, objects, parameters);
     window.requestAnimationFrame(function() { update(canvas, ctx, objects, parameters); });
 }
 
@@ -38,14 +38,21 @@ function simulate()
     canvas.height = window.innerHeight / 2;
 
     const MIN_SIZE = 20.0;
-    let scale = (Math.min(canvas.width, canvas.height)) / MIN_SIZE;
+    let cs = (Math.min(canvas.width, canvas.height)) / MIN_SIZE;
+    let gr = { x: 0.0, y: 10.0 };
 
-    const START = 0.5*scale;
-
-    ball = new Ball(START, {x: START, y: START}, 0.1, 'red');
+    ball = new Ball(0.5, {x: 2.0, y: 15.0}, (Math.PI / 5) + (3.0 * Math.PI / 2), 20.0, 'red');
 
     objects = [ball];
-    parameters = null;
+    parameters = 
+    { 
+        scale: cs, 
+        width: (canvas.width / cs),
+        height: (canvas.height / cs),
+        ts: (1.0 / 60.0),
+        gravity: gr,
+        damping: 0.8
+    };
 
     update(canvas, ctx, objects, parameters);
 }
